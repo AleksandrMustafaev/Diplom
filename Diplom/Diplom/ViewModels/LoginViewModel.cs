8d56d0dbd7651d1e60731be978c94f88ef85b55a
Alexandr Mustafaiev <amustik2000@gmail.com>
2021-05-22 16:11:13 +0300

    addToAzure

---
+++ Diplom/Diplom/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diplom.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName {get;set;}
+        public string Password { get; set; }
+        public string grant_type { get; set; }
+    }
+}
